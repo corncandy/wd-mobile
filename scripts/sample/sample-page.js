5cc4d1efa9484a0d33d12bc75b849a240d673a5e
Cloud Mu <muyunsong@gmail.com>
2016-07-24 12:41:56 +0800

    Add progress bar.

---
+++ scripts/sample/sample-page.js
@@ -0,0 +1,2 @@
+"use strict";$(function(){var t=0,r=$(".progress .fill"),s=$(".point-value");$(".raise-button").click(function(){t+=50,t>950&&(t=0),r.css("transform","rotate("+180*t/950+"deg)"),s.text(t)})});
+//# sourceMappingURL=sample-page.js.map
