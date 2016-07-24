141e663acf52248e5e6efca86e126ad33c1b5cae
Cloud Mu <muyunsong@gmail.com>
2016-07-24 12:41:56 +0800

    Add progress bar.

--- app/scripts/sample/sample-page.js
+++ app/scripts/sample/sample-page.js
@@ -1,5 +1,17 @@
 'use strict';
 
 $(function() {
+  var total = 950;
+  var current = 0;
+  var $progress = $('.progress .fill');
+  var $point = $('.point-value');
 
+  $('.raise-button').click(function() {
+    current += 50;
+    if (current > 950) {
+      current = 0;
+    }
+    $progress.css('transform', 'rotate(' + current * 180 / 950 + 'deg)');
+    $point.text(current);
+  });
 });
