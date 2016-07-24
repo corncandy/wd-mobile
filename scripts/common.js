1ae67f620926062e7ce351eecdd523082248c382
Cloud Mu <muyunsong@gmail.com>
2016-07-24 12:55:41 +0800

    Distribution Commit

---
+++ scripts/common.js
@@ -0,0 +1,2 @@
+"use strict";"addEventListener"in document&&document.addEventListener("DOMContentLoaded",function(){FastClick&&FastClick.attach(document.body)},!1);
+//# sourceMappingURL=common.js.map
