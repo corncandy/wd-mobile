1f3095f8ae7f8c857c592c67013ceef207570663
Cloud Mu <muyunsong@gmail.com>
2016-07-24 12:55:25 +0800

    Correct percent.

--- scripts/sample/sample-page.js
+++ scripts/sample/sample-page.js
@@ -1,2 +1,2 @@
-"use strict";$(function(){var t=0,r=$(".progress .fill"),s=$(".point-value");$(".raise-button").click(function(){t+=50,t>950&&(t=0),r.css("transform","rotate("+180*t/950+"deg)"),s.text(t)})});
+"use strict";$(function(){var t=350,e=950,r=600,s=350,i=$(".progress .fill"),n=$(".point-value").text(s);$(".raise-button").click(function(){s+=50,s>e&&(s=t),i.css("transform","rotate("+180*(s-350)/r+"deg)"),n.text(s)})});
 //# sourceMappingURL=sample-page.js.map
